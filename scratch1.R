fc8bee41d9d5070cfdfecd5cf2997fa57f16e88c
Sabyrd54 <51201684+Sabyrd54@users.noreply.github.com>
2019-11-07 13:37:40 -0800

    progress

--- scratch1.R
+++ scratch1.R
@@ -3,3 +3,31 @@
 # Download example file per "https://datacarpentry.org/R-genomics/02-starting-with-data.html"
 download.file("https://raw.githubusercontent.com/datacarpentry/R-genomics/gh-pages/data/Ecoli_metadata.csv",
               "data/Ecoli_metadata.csv")
+
+#Download data as a data frame
+metadata<- read.csv("data/Ecoli_metadata.csv") 
+
+#add in dply packages
+library(dplyr)
+
+#Practice functions of dplyr
+
+select(metadata,sample,clade,cit,genome_size)
+
+filter(metadata, cit=="plus")
+
+metadata %>%
+  filter(cit=="plus") %>% 
+  select(sample,generation,clade)  
+
+
+select(metadata, sample,clade, cit,genome_size)
+
+filter(metadata, clade=="plus")
+
+metadata %>%
+  filter(clade == "Cit+")
+  
+
+filter(metadata, clade == "Cit+")
+
