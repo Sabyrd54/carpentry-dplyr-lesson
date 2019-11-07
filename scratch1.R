c4a2f27c347a91d4228720738c5291cee11a8eee
Sabyrd54 <51201684+Sabyrd54@users.noreply.github.com>
2019-11-07 12:23:42 -0800

    starting repo

---
+++ scratch1.R
@@ -0,0 +1,5 @@
+#-- scratch1.R --------------------------------------------------------------#
+
+# Download example file per "https://datacarpentry.org/R-genomics/02-starting-with-data.html"
+download.file("https://raw.githubusercontent.com/datacarpentry/R-genomics/gh-pages/data/Ecoli_metadata.csv",
+              "data/Ecoli_metadata.csv")
